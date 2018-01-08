2a93b85a37b34b74dfaebf00ef2a10af752f3bfe
Edson Lek Hong Ma <edsonma@gmail.com>
2018-01-08 11:26:09 -0200

    Dockerizing react template and changed some titles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>This is a template create for react</h1>
         <ComponentOne />
         <hr></hr>
         <ComponentTwo />
